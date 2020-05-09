3b5b125b3f6868ef751832107d01dbb137d35bc5
Colin Dick <colindaviddick@outlook.com>
2020-05-09 17:01:07 +0100

    Quiz

---
+++ MusicQuiz/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization.Configuration;
+
+namespace MusicQuiz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+        private string rootPath = (@"C:\Users\Colin\source\repos\MusicQuiz\MusicQuiz\Themes\");
+        private string theme = (@"Test\");
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetAnswers();
+        }
+
+        private void PlaySound(string soundPath)
+        {
+            mediaPlayer.Open(new Uri(soundPath));
+            mediaPlayer.Play();
+        }
+
+        string[] answers = System.IO.File.ReadAllLines(@"C:\Users\Colin\source\repos\MusicQuiz\MusicQuiz\Themes\Test\Answers.txt");
+
+        // Create a dropdown menu to change the theme.
+
+        private void SetAnswers()
+        {
+            answers = System.IO.File.ReadAllLines(rootPath + theme + "Answers.txt");
+
+            Answer1.Text = answers[0];
+            Answer2.Text = answers[1];
+            Answer3.Text = answers[2];
+            Answer4.Text = answers[3];
+            Answer5.Text = answers[4];
+            Answer6.Text = answers[5];
+            Answer7.Text = answers[6];
+            Answer8.Text = answers[7];
+            Answer9.Text = answers[8];
+            Answer10.Text = answers[9];
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e){PlaySound(rootPath + theme + "1.mp3");}
+        private void Button2_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "2.mp3"); }
+        private void Button3_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "3.mp3"); }
+        private void Button4_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "4.mp3"); }
+        private void Button5_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "5.mp3"); }
+        private void Button6_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "6.mp3"); }
+        private void Button7_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "7.mp3"); }
+        private void Button8_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "8.mp3"); }
+        private void Button9_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "9.mp3"); }
+        private void Button10_Click(object sender, RoutedEventArgs e) { PlaySound(rootPath + theme + "10.mp3"); }
+
+        private void Button1Answer_Click(object sender, RoutedEventArgs e){if(Answer1.Visibility == Visibility.Hidden){Answer1.Visibility = Visibility.Visible;}else{Answer1.Visibility = Visibility.Hidden;}}
+        private void Button2Answer_Click(object sender, RoutedEventArgs e) { if (Answer2.Visibility == Visibility.Hidden) { Answer2.Visibility = Visibility.Visible; } else { Answer2.Visibility = Visibility.Hidden; } }
+        private void Button3Answer_Click(object sender, RoutedEventArgs e) { if (Answer3.Visibility == Visibility.Hidden) { Answer3.Visibility = Visibility.Visible; } else { Answer3.Visibility = Visibility.Hidden; } }
+        private void Button4Answer_Click(object sender, RoutedEventArgs e) { if (Answer4.Visibility == Visibility.Hidden) { Answer4.Visibility = Visibility.Visible; } else { Answer4.Visibility = Visibility.Hidden; } }
+        private void Button5Answer_Click(object sender, RoutedEventArgs e) { if (Answer5.Visibility == Visibility.Hidden) { Answer5.Visibility = Visibility.Visible; } else { Answer5.Visibility = Visibility.Hidden; } }
+        private void Button6Answer_Click(object sender, RoutedEventArgs e) { if (Answer6.Visibility == Visibility.Hidden) { Answer6.Visibility = Visibility.Visible; } else { Answer6.Visibility = Visibility.Hidden; } }
+        private void Button7Answer_Click(object sender, RoutedEventArgs e) { if (Answer7.Visibility == Visibility.Hidden) { Answer7.Visibility = Visibility.Visible; } else { Answer7.Visibility = Visibility.Hidden; } }
+        private void Button8Answer_Click(object sender, RoutedEventArgs e) { if (Answer8.Visibility == Visibility.Hidden) { Answer8.Visibility = Visibility.Visible; } else { Answer8.Visibility = Visibility.Hidden; } }
+        private void Button9Answer_Click(object sender, RoutedEventArgs e) { if (Answer9.Visibility == Visibility.Hidden) { Answer9.Visibility = Visibility.Visible; } else { Answer9.Visibility = Visibility.Hidden; } }
+        private void Button10Answer_Click(object sender, RoutedEventArgs e) { if (Answer10.Visibility == Visibility.Hidden) { Answer10.Visibility = Visibility.Visible; } else { Answer10.Visibility = Visibility.Hidden; } }
+
+        private void Game1_Selected(object sender, RoutedEventArgs e)
+        {
+            theme = (@"Game1\");
+            SetAnswers();
+    }
+
+        private void Test_Selected(object sender, RoutedEventArgs e)
+        {
+            theme = (@"Test\");
+            SetAnswers();
+        }
+
+        private void MovieQuiz1_Selected(object sender, RoutedEventArgs e)
+        {
+            theme = (@"MovieQuotes1\");
+            SetAnswers();
+        }
+    }
+}
